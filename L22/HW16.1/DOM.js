293cf93d6399419aca707d8ece2c800ac9f5900c
Sheliss <sheliss.kai@gmail.com>
2021-02-15 00:31:10 +0200

    wip1

--- L22/HW16.1/DOM.js
+++ L22/HW16.1/DOM.js
@@ -14,15 +14,19 @@ window.addEventListener('load', () => {
             image: 'https://images-na.ssl-images-amazon.com/images/I/616MVaXD29L._AC_SX679_.jpg'
         },
         {
-            id: "id-2",
+            id: "id-3",
             name: 'Title 3',
-            price: '300',
+            price: '500',
             image: 'https://images-na.ssl-images-amazon.com/images/I/616MVaXD29L._AC_SX679_.jpg'
         }
     ];
-    let totalCost = 0;
+
+
     
     const productsContainer =  document.querySelector('.products');
+    const cartContainer = document.querySelector('.product_quantity');
+    let productQuantity = localStorage.productCart === undefined ? {} : JSON.parse(localStorage.productCart);
+    
 
     productsContainer.innerHTML = `${products.map(item => {
         let name = item.name;
@@ -43,8 +47,10 @@ window.addEventListener('load', () => {
     `;
     }).join('')}`;
 
+    
     const buttons = productsContainer.querySelectorAll('button.product__action');
     const totalContainer = document.querySelector('#total');
+    totalContainer.innerHTML = localStorage.totalPrice === undefined ? 0 : JSON.parse(localStorage.totalPrice);
 
     buttons.forEach(btn => {
         btn.addEventListener('click', onButtonAddClick)
@@ -53,10 +59,41 @@ window.addEventListener('load', () => {
     function onButtonAddClick(event) {
         const productId = event.target.dataset.productId;
         const currentProduct = products.find(item => item.id == productId);
+        const storageCost = localStorage.totalPrice === undefined ? 0 : JSON.parse(localStorage.totalPrice);
+        localStorage.totalPrice = storageCost + Number(currentProduct.price);
+        totalContainer.innerHTML = localStorage.totalPrice;
+        const localProductQuantity = productQuantity[currentProduct.id] === undefined ? 0 : productQuantity[currentProduct.id];
+        productQuantity[currentProduct.id] = localProductQuantity + 1;
+        localStorage.productCart = JSON.stringify(productQuantity);
 
-        totalCost += Number(currentProduct.price);
-        totalContainer.innerHTML = totalCost;
+        cartContainer.innerHTML = /*`${productQuantity.map(() => {
+            if(productQuantity[currentProduct.id] != 0 ) {
+            return `
+                <div class="cart__item">
+                    <div class="cart__name">${currentProduct.name}</div>
+                </div>
+            `;
+        }
+        }).join('')}`;*/
+
+        `${Object.entries(productQuantity).map(item => {
+            
+                return `
+                <div class="cartItem">
+                    ${item}
+                </div>
+                `
+            
+            
+            
+        }).join('')}
+        `;
+
+
+        
+
+
+         
     }
 
-    console.log(buttons);
 })
